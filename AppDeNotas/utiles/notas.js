23c4943b5bf7b3e39138ae33d5b52ded542d9ece
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-05-20 16:23:16 -0600

    App de notas finalizada

---
+++ AppDeNotas/utiles/notas.js
@@ -0,0 +1,81 @@
+import fs from 'fs'
+import chalk from 'chalk'
+
+const cargarNotas = () => {
+    try {
+        const dataBuffer = fs.readFileSync("notas.json")
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch {
+        return [];
+    }
+}
+
+const guardarNotas = (notas) => {
+    const dataJson = JSON.stringify(notas);
+    fs.writeFileSync("notas.json", dataJson);
+}
+
+const agregarNotas = (title, body) => {
+    const notas = cargarNotas();
+    const duplicateNote = notas.find((note) => {
+      note.title === title;
+    });
+  
+    if (!duplicateNote) {
+      notas.push({
+        title,
+        body,
+      });
+      guardarNotas(notas);
+      console.log(chalk.green.inverse("nota agregada"));
+    } else {
+      console.log(chalk.red.inverse("Nota no agregada"));
+    }
+  };
+
+const eliminarNotas = (title) =>{
+    const notas = cargarNotas();
+    const notasToKeep = notas.filter((note) => note.title !== title)
+
+    if(notas.length > notasToKeep   .length){
+        guardarNotas(notasToKeep)
+        console.log(chalk.green.inverse('Nota elminada'))
+    }else{
+        console.log(chalk.red.inverse('Nota no eliminada'))
+    }
+}
+
+const listarNotas = () => {
+    const notas = cargarNotas();
+    console.log(chalk.inverse("Tus notas"))
+    notas.forEach((element) => {
+        console.log(element.title)
+    });
+}
+
+const leerNotas = (title) => {
+    const notas = cargarNotas();
+    const note = notas.find((note) => note.title === title)
+
+    if(note){
+        console.log(chalk.inverse(note.title))
+        console.log(chalk.blue(note.body))
+    }else{
+        console.log(chalk.red.inverse('Nota no encontrada'))
+    }
+}
+
+const editarNotas = (title, nuevaNota) => {
+    const notas = cargarNotas();
+    const note = notas.find((note) => note.title === title);
+    if(note) {
+        note.body = nuevaNota;
+        guardarNotas(notas);
+        console.log(chalk.green.inverse('La nota se modificó correctamente'));
+    } else {
+        console.log(chalk.red.inverse('Nota no encontrada'));
+    }
+};
+
+export{ agregarNotas, eliminarNotas, listarNotas, leerNotas, editarNotas }
